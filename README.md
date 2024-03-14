bbd64e9ce369975479808530c5a2a53ab00e20da
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-03-14 11:28:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OSHI-WMIC-Hardware-Info
+ A standard GUI Hardware and Network Info tool that uses WMIC and OSHI to fetch the information
---
+++ src/com/egg/wmic/main/CPU_Info_WMIC.java
@@ -0,0 +1,87 @@
+package com.egg.wmic.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class CPU{
+	private static Process cpu;
+	
+	private static String getValues(String WMIC_Attribute) throws IOException {
+		String[] command = {"cmd", "/c", "wmic cpu get "+WMIC_Attribute+" /format:list"};
+		cpu = Runtime.getRuntime().exec(command);
+		BufferedReader br = new BufferedReader(new InputStreamReader(cpu.getInputStream()));
+		
+		String currentLine;
+		String actualName = null;
+		
+		while((currentLine=br.readLine())!=null)
+			if(!currentLine.isBlank() || !currentLine.isEmpty())
+				actualName = currentLine;
+		
+		br.close();
+		return actualName.substring(actualName.indexOf("=")+1);
+	}
+	
+	public static String getName() throws IOException {
+		return getValues("Name");
+	}
+	
+	public static String getCores() throws IOException {
+		return getValues("NumberOfCores");
+	}
+	
+	public static String getThreads() throws IOException {
+		return getValues("ThreadCount");
+	}
+	
+	public static String getManufacturer() throws IOException{
+		return getValues("Manufacturer");
+	}
+	
+	public static String getAddressWidth() throws IOException{
+		return getValues("AddressWidth");
+	}
+	
+	public static String getL2CacheSize() throws IOException{
+		return getValues("L2CacheSize");
+	}
+	
+	public static String getL3CacheSize() throws IOException{
+		return getValues("L3CacheSize");
+	}
+	
+	public static String getCurrentClockSpeed() throws IOException{
+		return getValues("CurrentClockSpeed");
+	}
+	
+	public static String getMaxClockSpeed() throws IOException{
+		return getValues("MaxClockSpeed");
+	}
+	
+	public static String getBusSpeed() throws IOException{
+		return getValues("ExtClock");
+	}
+	
+	public static String getSocket() throws IOException{
+		return getValues("SocketDesignation");
+	}
+}
+
+public class CPU_Info_WMIC {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(CPU.getName());
+		System.out.println(CPU.getCores());
+		System.out.println(CPU.getThreads());
+		System.out.println(CPU.getManufacturer());
+		System.out.println(CPU.getAddressWidth()+" bit");
+		System.out.println(CPU.getL2CacheSize()+" KB");
+		System.out.println(CPU.getL3CacheSize()+" KB");
+		System.out.println(CPU.getCurrentClockSpeed()+" MHz");
+		System.out.println(CPU.getMaxClockSpeed()+" MHz");
+		System.out.println(CPU.getBusSpeed());
+		System.out.println(CPU.getSocket());
+	}
+
+}
