a5fe211f27bd45d910c7176ba76873b5e503ca71
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-03-15 21:59:41 +0530

    Update CPU.java

--- src/com/egg/wmic/main/CPU.java
+++ src/com/egg/wmic/main/CPU.java
@@ -22,48 +22,4 @@ class CPU{
 		br.close();
 		return actualName.substring(actualName.indexOf("=")+1).strip();
 	}
-	
-//	public static String getName() throws IOException {
-//		return getValues("Name");
-//	}
-//	
-//	public static String getCores() throws IOException {
-//		return getValues("NumberOfCores");
-//	}
-//	
-//	public static String getThreads() throws IOException {
-//		return getValues("ThreadCount");
-//	}
-//	
-//	public static String getManufacturer() throws IOException{
-//		return getValues("Manufacturer");
-//	}
-//	
-//	public static String getAddressWidth() throws IOException{
-//		return getValues("AddressWidth");
-//	}
-//	
-//	public static String getL2CacheSize() throws IOException{
-//		return getValues("L2CacheSize");
-//	}
-//	
-//	public static String getL3CacheSize() throws IOException{
-//		return getValues("L3CacheSize");
-//	}
-//	
-//	public static String getCurrentClockSpeed() throws IOException{
-//		return getValues("CurrentClockSpeed");
-//	}
-//	
-//	public static String getMaxClockSpeed() throws IOException{
-//		return getValues("MaxClockSpeed");
-//	}
-//	
-//	public static String getBusSpeed() throws IOException{
-//		return getValues("ExtClock");
-//	}
-//	
-//	public static String getSocket() throws IOException{
-//		return getValues("SocketDesignation");
-//	}
 }
