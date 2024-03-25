4173c885ab707f1ad990ce780ba305398be04e6e
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-03-25 19:47:41 +0530

    Update Win32_NetworkAdapterConfiguration.java
    Minor refactor

--- src/com/egg/system/networking/Win32_NetworkAdapterConfiguration.java
+++ src/com/egg/system/networking/Win32_NetworkAdapterConfiguration.java
@@ -11,8 +11,8 @@ public class Win32_NetworkAdapterConfiguration {
 		throw new IllegalStateException("Utility Class");
 	}
 	
-	public static Map<String,String> getAdapterConfiguration(String deviceID) throws IOException {
-		String[] command = {"powershell.exe", "/c", "Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$_.Index -eq '"+deviceID+"'} | Select-Object DHCPLeaseObtained, DHCPLeaseExpires, DHCPEnabled, DHCPServer, DNSHostName, DNSServerSearchOrder, IPAddress, IPEnabled, DefaultIPGateway, IPSubnet | Format-List"};
+	public static Map<String,String> getAdapterConfiguration(String adapterIndex) throws IOException {
+		String[] command = {"powershell.exe", "/c", "Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$_.Index -eq '"+adapterIndex+"'} | Select-Object DHCPLeaseObtained, DHCPLeaseExpires, DHCPEnabled, DHCPServer, DNSHostName, DNSServerSearchOrder, IPAddress, IPEnabled, DefaultIPGateway, IPSubnet | Format-List"};
 		
 		Process process = Runtime.getRuntime().exec(command);
 		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
