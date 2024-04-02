d2833fae10ab7a2775820d4ce81755332eb018b3
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-04-02 11:53:08 +0530

    Create Report.java
    Test for Report Generation

---
+++ src/com/egg/tests/Report.java
@@ -0,0 +1,11 @@
+package com.egg.tests;
+
+import com.egg.system.report.AIOReportGeneration;
+
+public class Report {
+
+	public static void main(String[] args) {
+		AIOReportGeneration.generate();
+	}
+
+}
