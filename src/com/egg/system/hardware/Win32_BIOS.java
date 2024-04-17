8bbb872671c15e9b96877b82c8fe5efe2da9d70c
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-04-17 11:42:20 +0530

    Refactor Win32_Baseboard and Win32_BIOS

--- src/com/egg/system/hardware/Win32_BIOS.java
+++ src/com/egg/system/hardware/Win32_BIOS.java
@@ -1,65 +1,18 @@
 package com.egg.system.hardware;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.egg.system.logger.ErrorLog;
+import com.egg.formatter.cim.CIM_ML;
 
 public class Win32_BIOS {
 	private static String classname = "Win32_BIOS";
+	private static String attributes = "Name, Caption, Manufacturer, ReleaseDate, SMBIOSPResent, Status, Version, CurrentLanguage, SMBIOSBIOSVersion";
 	private Win32_BIOS() {
 		throw new IllegalStateException("Utility Class");
 	}
 	
 	public static Map<String, String> getPrimaryBIOS() throws IOException, IndexOutOfBoundsException {
-		String methodName = "getPrimaryBIOS()";
-		String[] command = {"powershell.exe", "/c", " Get-CimInstance -ClassName Win32_BIOS | Where-Object {$_.PrimaryBIOS -eq 'True'} |Select-Object Name, Caption, Manufacturer, ReleaseDate, SMBIOSPResent, Status, Version, CurrentLanguage, SMBIOSBIOSVersion | Format-List"};
-		
-		Process process = Runtime.getRuntime().exec(command);
-		try {
-			int exitCode = process.waitFor();
-			if(exitCode!=0) {
-				BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-				String errorLine;
-     			List<String> errorList = new ArrayList<>();
-				
-				while((errorLine=error.readLine())!=null)
-					if(!errorLine.isBlank() || !errorLine.isEmpty())
-						errorList.add(errorLine);
-				
-				error.close();
-				ErrorLog errorLog = new ErrorLog();
-				
-				errorLog.log("\n"+classname+"-"+methodName+"\n"+errorList.toString()+"\nProcess Exited with code:"+exitCode+"\n");
-				return Collections.emptyMap();
-			}
-		}catch (InterruptedException e) {
-			ErrorLog errorLog = new ErrorLog();
-			errorLog.log("\n"+classname+"-"+methodName+"\n"+e.getMessage()+"\n\n");
-			Thread.currentThread().interrupt();
-		}
-		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		
-		String currentLine;
-		Map<String, String> propertyValues = new LinkedHashMap<>();
-		
-		while((currentLine=br.readLine())!=null)
-			if(!currentLine.isBlank() || !currentLine.isEmpty()) {
-				String key = "";
-				String value = "";
-				if(currentLine.contains(" : "))
-					propertyValues.put(key=currentLine.substring(0, currentLine.indexOf(":")).strip(), value =currentLine.substring(currentLine.indexOf(":")+1).strip());
-				else
-					propertyValues.replace(key, value.concat(currentLine.strip()));
-			}
-		br.close();
-		
-		return propertyValues;
+		return CIM_ML.getWhere(classname, "PrimaryBIOS", "True", attributes);
 	}
 }
