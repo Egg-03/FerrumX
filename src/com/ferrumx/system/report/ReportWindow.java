912e7b36e5e1749a73d2ccfe781b1154176146c5
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-05-12 10:32:42 +0530

    Update ReportWindow.java
    Bump version to v1.2.1

--- src/com/ferrumx/system/report/ReportWindow.java
+++ src/com/ferrumx/system/report/ReportWindow.java
@@ -72,7 +72,7 @@ public class ReportWindow extends JFrame {
 	 */
 	private ReportWindow() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(ReportWindow.class.getResource("/resources/icon_main.png")));
-		setTitle("FerrumX Report Tool v1.2.0");
+		setTitle("FerrumX Report Tool v1.2.1");
 		setResizable(false);
 		setAlwaysOnTop(false);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
