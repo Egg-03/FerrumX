39ef479684181834427185b83bb0f092b0f7778b
Egg-03 <sayanbhattacharjee2001@gmail.com>
2024-06-06 10:53:50 +0530

    Update HardwareID.java
    Made use of the AutoCloseable interface implemented by the ExecutorService

--- src/com/ferrumx/system/hardware/HardwareID.java
+++ src/com/ferrumx/system/hardware/HardwareID.java
@@ -26,25 +26,32 @@ public class HardwareID {
 	 */
 	public static String getHardwareID() throws ExecutionException, InterruptedException {
 		
-		ExecutorService EXEC = Executors.newFixedThreadPool(7);
+		String cpuName = "N/A";
+		String cpuId = "N/A";
+		String motherBoardName = "N/A";
+		String deviceName = "N/A";
+		String userName = "N/A";
+		int countRAM = 0;
+		int countStorage = 0;
+
 		
-		Future<String> cpuNameTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "Name"));
-		Future<String> cpuIdTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "ProcessorID"));
-		Future<String> motherBoardNameTask = EXEC.submit(()-> CIM_SL.get("Win32_BaseBoard", "Product"));
-		Future<String> deviceNameTask = EXEC.submit(()-> CIM_SL.get("Win32_OperatingSystem", "CSName"));
-		Future<String> userNameTask = EXEC.submit(User::getUsername);
-		Future<Integer> countRAMTask = EXEC.submit(()-> Win32_PhysicalMemory.getTag().size());
-		Future<Integer> countStorageTask = EXEC.submit(()-> Win32_DiskDrive.getDriveID().size());
-		
-		String cpuName = cpuNameTask.get();
-		String cpuId = cpuIdTask.get();
-		String motherBoardName = motherBoardNameTask.get();
-		String deviceName = deviceNameTask.get();
-		String userName = userNameTask.get();
-		int countRAM = countRAMTask.get();
-		int countStorage = countStorageTask.get();
-		
-		EXEC.shutdown();
+		try(ExecutorService EXEC = Executors.newFixedThreadPool(7);) {
+			Future<String> cpuNameTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "Name"));
+			Future<String> cpuIdTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "ProcessorID"));
+			Future<String> motherBoardNameTask = EXEC.submit(()-> CIM_SL.get("Win32_BaseBoard", "Product"));
+			Future<String> deviceNameTask = EXEC.submit(()-> CIM_SL.get("Win32_OperatingSystem", "CSName"));
+			Future<String> userNameTask = EXEC.submit(User::getUsername);
+			Future<Integer> countRAMTask = EXEC.submit(()-> Win32_PhysicalMemory.getTag().size());
+			Future<Integer> countStorageTask = EXEC.submit(()-> Win32_DiskDrive.getDriveID().size());
+			
+			cpuName = cpuNameTask.get();
+			cpuId = cpuIdTask.get();
+			motherBoardName = motherBoardNameTask.get();
+			deviceName = deviceNameTask.get();
+			userName = userNameTask.get();
+			countRAM = countRAMTask.get();
+			countStorage = countStorageTask.get();
+		}
 		return userName+"/"+deviceName+"/"+cpuName+"/"+cpuId+"/"+motherBoardName+"/"+countRAM+"/"+countStorage;
 	}
 }
