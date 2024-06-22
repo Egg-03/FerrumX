1ece96decad2e834d24a368fc1a8f8e7d8a4a407
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-06-22 22:38:34 +0530

    Reapply "Bump version to v1.2.4"
    This reverts commit 66deb2e928e233d4b18530aea31f02f5d5b91f49.

--- src/com/ferrumx/system/hardware/HardwareID.java
+++ src/com/ferrumx/system/hardware/HardwareID.java
@@ -1,21 +1,44 @@
 package com.ferrumx.system.hardware;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import com.ferrumx.formatter.cim.CIM_ML;
 import com.ferrumx.formatter.cim.CIM_SL;
-import com.ferrumx.system.currentuser.User;
 
 /**
- * Hardware ID generation class based on the following format : "User-name/DeviceName/CPU/CPUID/MotherboardName/RAM Count/Storage Count"
+ * Hardware ID generation class based on the following format : "CPUName/CPUID/MotherboardName/DriveIDs"
  * @author Egg-03
- * @version 1.1.0
+ * @version 1.2.4
  */
 public class HardwareID {
 	protected HardwareID() {
 		throw new IllegalStateException("Utility Class");
+	}
+	/**
+	 * Uses {@link com.ferrumx.formatter.cim.CIM_ML#getIDWhere(String, String, String, String)} to fetch IDE and SCSI Interface Type Disk IDs
+	 * @return a concatenated list of all IDE and SCSI drive IDs currently installed
+	 * @throws IndexOutOfBoundsException if there is a parsing error incurred during extracting the IDs
+	 * @throws IOException in case of any IOException thrown by the underlying parser
+	 */
+	private static String getDiskSerials() throws IndexOutOfBoundsException, IOException {
+		List<String> ideInterface = CIM_ML.getIDWhere("Win32_DiskDrive", "InterfaceType", "IDE", "SerialNumber");
+		List<String> scsiInterface = CIM_ML.getIDWhere("Win32_DiskDrive", "InterfaceType", "SCSI", "SerialNumber");
+		
+		StringBuilder ideDrives = new StringBuilder("");
+		StringBuilder scsiDrives = new StringBuilder("");
+		
+		for(String ide:ideInterface)
+			ideDrives.append(ide);
+		
+		for(String scsi:scsiInterface)
+			scsiDrives.append(scsi);
+		
+		return ideDrives.toString()+scsiDrives.toString();
 	}
 	
 	/**
@@ -29,29 +52,19 @@ public class HardwareID {
 		String cpuName = "N/A";
 		String cpuId = "N/A";
 		String motherBoardName = "N/A";
-		String deviceName = "N/A";
-		String userName = "N/A";
-		int countRAM = 0;
-		int countStorage = 0;
+		String driveId = "N/A";
 
-		
-		try(ExecutorService EXEC = Executors.newFixedThreadPool(7);) {
+		try(ExecutorService EXEC = Executors.newFixedThreadPool(4);) {
 			Future<String> cpuNameTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "Name"));
 			Future<String> cpuIdTask = EXEC.submit(()-> CIM_SL.get("Win32_Processor", "ProcessorID"));
 			Future<String> motherBoardNameTask = EXEC.submit(()-> CIM_SL.get("Win32_BaseBoard", "Product"));
-			Future<String> deviceNameTask = EXEC.submit(()-> CIM_SL.get("Win32_OperatingSystem", "CSName"));
-			Future<String> userNameTask = EXEC.submit(User::getUsername);
-			Future<Integer> countRAMTask = EXEC.submit(()-> Win32_PhysicalMemory.getTag().size());
-			Future<Integer> countStorageTask = EXEC.submit(()-> Win32_DiskDrive.getDriveID().size());
+			Future<String> driveIdTask = EXEC.submit(HardwareID::getDiskSerials);
 			
 			cpuName = cpuNameTask.get();
 			cpuId = cpuIdTask.get();
 			motherBoardName = motherBoardNameTask.get();
-			deviceName = deviceNameTask.get();
-			userName = userNameTask.get();
-			countRAM = countRAMTask.get();
-			countStorage = countStorageTask.get();
+			driveId = driveIdTask.get();	
 		}
-		return userName+"/"+deviceName+"/"+cpuName+"/"+cpuId+"/"+motherBoardName+"/"+countRAM+"/"+countStorage;
+		return cpuName+"/"+cpuId+"/"+motherBoardName+"/"+driveId;
 	}
 }
