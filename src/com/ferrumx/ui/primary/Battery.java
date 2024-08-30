25fc5d00f15f6ea4149dad5889d432a0a5668705
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-08-30 09:48:16 +0530

    Update Battery.java
    Added Battery Status and Chemistry Interpreters

--- src/com/ferrumx/ui/primary/Battery.java
+++ src/com/ferrumx/ui/primary/Battery.java
@@ -22,21 +22,73 @@ final class Battery {
 			if(!battery.isEmpty()) {
 				batteryFields[0].setText(battery.get("Caption"));
 				batteryFields[1].setText(battery.get("Status"));
-				batteryFields[2].setText(battery.get("BatteryStatus"));
-				batteryFields[3].setText(battery.get("Chemistry"));
-				batteryFields[4].setText(battery.get("EstimatedChargeRemaining"));
-				batteryFields[5].setText(battery.get("EstimatedRunTime"));
+				batteryFields[2].setText(batteryStatusInterpreter(battery.get("BatteryStatus")));
+				batteryFields[3].setText(batteryChemistryInterpreter(battery.get("Chemistry")));
+				batteryFields[4].setText(battery.get("EstimatedChargeRemaining")+"%");
+				batteryFields[5].setText(battery.get("EstimatedRunTime")+" min.");
 			}
 			
 			if(!portableBattery.isEmpty()) {
 				batteryFields[6].setText(portableBattery.get("Name"));
 				batteryFields[7].setText(portableBattery.get("DeviceID"));
-				batteryFields[8].setText(portableBattery.get("DesignCapacity"));
-				batteryFields[9].setText(portableBattery.get("DesignVoltage"));
+				batteryFields[8].setText(portableBattery.get("DesignCapacity")+" mWh");
+				batteryFields[9].setText(portableBattery.get("DesignVoltage")+ "mV");
 			}
 		} catch (IndexOutOfBoundsException | IOException e) {
 			new ExceptionUI("Battery Initialization Error", e.getMessage()).setVisible(true);
 		}
 		
+	}
+	
+	private static String batteryStatusInterpreter (String charge) {
+		switch (charge) {
+		case "1":
+			return "Discharging";
+		case "2":
+			return "Unknown";
+		case "3":
+			return "Fully Charged";
+		case "4":
+			return "Low";
+		case "5":
+			return "Critical";
+		case "6":
+			return "Charging";
+		case "7":
+			return "Charging and High";
+		case "8":
+			return "Charging and Low";
+		case "9":
+			return "Charging and Critical";
+		case "10":
+			return "Undefined";
+		case "11":
+			return "Partially Charged";
+		default:
+			return charge;
+		}
+	}
+	
+	private static String batteryChemistryInterpreter (String chemistry) {
+		switch (chemistry) {
+		case "1":
+			return "Other";
+		case "2":
+			return "Unknown";
+		case "3":
+			return "Lead Acid";
+		case "4":
+			return "Nickel Cadmium";
+		case "5":
+			return "Nickel MetalHydride";
+		case "6":
+			return "Lithium-ion";
+		case "7":
+			return "Zinc Air";
+		case "8":
+			return "Lithium Polymer";
+		default:
+			return chemistry;
+		}
 	}
 }
