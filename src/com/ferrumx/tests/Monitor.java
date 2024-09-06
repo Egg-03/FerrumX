f16300ea428631cecbd443abf6826371d8cf8c1b
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-09-06 11:56:06 +0530

    Create Win32_DesktopMonitor

---
+++ src/com/ferrumx/tests/Monitor.java
@@ -0,0 +1,26 @@
+package com.ferrumx.tests;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.ferrumx.exceptions.ShellException;
+import com.ferrumx.system.hardware.Win32_DesktopMonitor;
+
+public class Monitor {
+	public static void main(String[] args)
+			throws IOException, IndexOutOfBoundsException, ShellException, InterruptedException {
+		List<String> deviceIDs = Win32_DesktopMonitor.getMonitorID();
+		Map<String, String> currentMonitor;
+
+		System.out.println(deviceIDs);
+
+		for (String currentID : deviceIDs) {
+			currentMonitor = Win32_DesktopMonitor.getMonitorProperties(currentID);
+			for (Map.Entry<String, String> entry : currentMonitor.entrySet()) {
+				System.out.println(entry.getKey() + ": " + entry.getValue());
+			}
+			System.out.println();
+		}
+	}
+}
