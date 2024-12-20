64c58bc3d9ed5ee79cce42b30665220a7937ed13
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-12-20 13:24:42 +0530

    Update ShellException
    Description for the constructor

--- src/com/ferrumx/exceptions/ShellException.java
+++ src/com/ferrumx/exceptions/ShellException.java
@@ -8,7 +8,11 @@ package com.ferrumx.exceptions;
  */
 public class ShellException extends Exception{
 	private static final long serialVersionUID = 5805343712198045667L;
-
+	
+	/**
+	 * Constructor for ShellException
+	 * @param message The message to be displayed when the exception is thrown
+	 */
 	public ShellException(String message) {
 		super(message);
 	}
