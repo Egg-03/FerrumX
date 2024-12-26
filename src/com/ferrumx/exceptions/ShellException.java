081e5e5e91c8379a6b9307c6ce47c0b165ef37a0
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2024-12-26 10:19:58 +0530

    Update ShellException.java
    Removed the version tag from Javadoc

--- src/com/ferrumx/exceptions/ShellException.java
+++ src/com/ferrumx/exceptions/ShellException.java
@@ -4,7 +4,6 @@ package com.ferrumx.exceptions;
  * This is a custom exception class that is thrown when there are errors from the powershell's output
  *
  * @author Egg-03
- * @version 1.3.0
  */
 public class ShellException extends Exception{
 	private static final long serialVersionUID = 5805343712198045667L;
